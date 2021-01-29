401e6d61adabec1f31963a54098341b3b7904eba
Xessen <46282429+Xessen@users.noreply.github.com>
2021-01-29 18:30:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from math import prod
 
-def numbercaesar(secretcode):
-    print(prod(map(int,str(secretcode))))
-
 def remaining(number,limit):
     while number>limit:
         number=number%limit
@@ -10,19 +7,22 @@ def remaining(number,limit):
 def encoder():
     secretcode=int(input())
     secretcode_2=secretcode
+    
     #text=input()
     alph={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'J',11:'K',12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T',21:'U',22:'V',23:'W',24:'X',25:'Y',26:'Z'}
     rev_alph={v: k for k, v in alph.items()}
     beth=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     count=0
-
+    #test.txt yerine kullanıcıdan alınan text değişkenini yaz
     with open('test.txt') as f:
         f1=open("text1","w+")
         print('aa')
+        #4800 yerine textin uzunluğunu al ama len(f.read()) yapınca garip bi şekilde bozuluyor onu hallet
         while count<4800:
             print("bb")
             if secretcode!=0:
                 change=prod(map(int,str(secretcode)))
+                #şu debug printlerini at
                 print("cc")
 
                 for line in f.read():
@@ -45,4 +45,5 @@ def encoder():
 
 encoder()      
             
+#decoderi hallet
     
